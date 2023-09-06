57267257280a34f016c2af0cb182b93b4cec52da
Dell-User <Dell-User@DESKTOP-B16SGH2>
2023-09-06 11:42:34 +0200

    Add project files.

---
+++ Complex Arithmetics Sept 2023/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'Declaring variables
+        Dim dPricePerUnit As Decimal
+        Dim dQuantity As Decimal
+        Dim dDiscountPerUnit As Decimal
+        Dim dDiscoutedAmount As Decimal
+        Dim dVAT As Decimal
+        Dim dTotalAmountExVAT As Decimal
+        Dim dTotalAmountIncVAT As Decimal
+
+        dPricePerUnit = 10
+        dQuantity = 10
+        dDiscountPerUnit = 2
+        dVAT = 0.15
+        dDiscoutedAmount = dDiscountPerUnit * dQuantity
+        dTotalAmountExVAT = (dPricePerUnit * dQuantity) - dDiscoutedAmount
+        dTotalAmountIncVAT = (dTotalAmountExVAT * dVAT) + dTotalAmountExVAT
+
+        'Print the Vat excluded amount
+        MessageBox.Show("Total Exc. VAT:" & " " & dTotalAmountExVAT)
+
+        'Print the Vat included amount
+        MessageBox.Show("Total Inc. VAT:" & " " & dTotalAmountIncVAT)
+    End Sub
+End Class
---
+++ Complex Arithmetics Sept 2023/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Complex Arithmetics Sept 2023")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Complex Arithmetics Sept 2023")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f2dfa6d1-a721-4b10-8486-730b18f3e108")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
